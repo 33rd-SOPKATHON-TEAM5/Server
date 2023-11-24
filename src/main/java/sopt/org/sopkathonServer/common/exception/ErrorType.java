d6f8ee9d3fadd21c1ddd27ebae184c008c13329f
jun02160 <jun020216@naver.com>
2023-11-24 12:48:19 +0900

    [CHORE] 프로젝트 초기 세팅 #1

---
+++ src/main/java/sopt/org/sopkathonServer/common/exception/ErrorType.java
@@ -0,0 +1,23 @@
+package sopt.org.sopkathonServer.common.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public enum ErrorType {
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다"),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public final int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+}
---
+++ src/main/java/sopt/org/sopkathonServer/common/exception/SuccessType.java
@@ -0,0 +1,23 @@
+package sopt.org.sopkathonServer.common.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public enum SuccessType {
+
+    HEALTH_CHECK_SUCCESS(HttpStatus.OK, "헬스 체크에 성공했습니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public final int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+}
