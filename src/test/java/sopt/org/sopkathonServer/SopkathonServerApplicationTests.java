bbf4bfbe545530e9b52de69510b50c904c348dc7
jun02160 <jun020216@naver.com>
2023-11-24 10:53:44 +0900

    [CHORE] 프로젝트 초기 세팅

---
+++ src/test/java/sopt/org/sopkathonServer/SopkathonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.sopkathonServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkathonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
