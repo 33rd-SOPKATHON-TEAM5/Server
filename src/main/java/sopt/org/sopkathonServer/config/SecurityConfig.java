41b591b6858e083b34b84c005c5f71d1872f9baa
jun02160 <jun020216@naver.com>
2023-11-26 01:15:06 +0900

    🐛 bugfix: Security 관련 설정 수정 #7

--- src/main/java/sopt/org/sopkathonServer/config/SecurityConfig.java
+++ src/main/java/sopt/org/sopkathonServer/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package sopt.org.sopkathonServer.config;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,13 +15,7 @@ public class SecurityConfig {
         return http
                 .csrf().disable()
                 .formLogin().disable()
-                .httpBasic().disable()
-
-                .authorizeHttpRequests()
-                .requestMatchers("/cry").permitAll()
-                .anyRequest().authenticated()  // WHITE_LIST에 포함되지 않는 나머지 요청은 모두 인증 필요!
-
-                .and()
+                .httpBasic().disable()   // Security 설정 제외
                 .build();   // -> 스프링 빈으로 등록, 어플리케이션 실행 시 자동으로 필터에 등록된다!
     }
 }
